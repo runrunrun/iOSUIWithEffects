9d5549b1f11655469b6735e036cc0c4ecd80ea88
runrunrun <harikunwar@gmail.com>
2013-05-02 22:14:59 -0700

    Sliding overlay and reveal button underneath.
    Still contains some bugs. But main physics is done.

--- SlidingTableViewCell/SlidingTableViewCell/OverlayCell.m
+++ SlidingTableViewCell/SlidingTableViewCell/OverlayCell.m
@@ -9,15 +9,17 @@
 #import "OverlayCell.h"
 #import <QuartzCore/QuartzCore.h>
 
-#define PARTIAL_OVERLAY_WIDTH 50.0f
-#define OVERLAY_MAX_TRANSLATION 100.0F
+#define OVERLAY_PIN_POSITION_X  70
 
 @interface OverlayCell ()
 
 @property (nonatomic, strong) UIView *overlayView;
-@property (nonatomic, assign) BOOL isFullOverlay;
+@property (nonatomic, strong) UIButton *buttonOne;
+@property (nonatomic, strong) UIButton *buttonTwo;
+@property (nonatomic, assign) BOOL isOverlayPinned;
 @property (nonatomic, assign) CGFloat firstX;
 @property (nonatomic, assign) CGFloat firstY;
+
 
 @end
 
@@ -27,13 +29,20 @@
 {
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
     if (self) {
+        
+        
+        [self setupButtons];
+        
+        [self addSubview:_buttonOne];
+        [self addSubview:_buttonTwo];
 
+        
         [self setupOverlay];
         [self addSubview:_overlayView];
         
         [self setupDescriptionLabel];
         [_overlayView addSubview:_descriptionLabel];
-
+        
     }
     return self;
 }
@@ -48,6 +57,20 @@
     [super layoutSubviews];
     
     _descriptionLabel.frame = [self descriptionLabelFrame];
+    _buttonOne.frame = [self buttonOneFrame];
+    _buttonTwo.frame = [self buttonTwoFrame];
+}
+
+- (void)setupButtons
+{
+    _buttonOne = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    _buttonTwo = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    
+    [_buttonOne addTarget:self action:@selector(buttonAction:) forControlEvents:UIControlEventTouchUpInside];
+    [_buttonTwo addTarget:self action:@selector(buttonAction:) forControlEvents:UIControlEventTouchUpInside];
+    
+    _buttonOne.tag = 1;
+    _buttonTwo.tag = 2;
 }
 
 - (void)setupOverlay
@@ -100,68 +123,72 @@
     return _overlayView.bounds;
 }
 
+- (CGRect)buttonOneFrame
+{
+    CGFloat width = 20.0f;
+    CGFloat height = self.bounds.size.height;
+    CGFloat x = 5.0f;
+    CGFloat y = 0.0f;
+    
+    return CGRectMake(x, y, width, height);
+}
+
+- (CGRect)buttonTwoFrame
+{
+    CGFloat width = 20.0f;
+    CGFloat height = self.bounds.size.height;
+    CGFloat x = CGRectGetMaxX(_buttonOne.frame) + 10;
+    CGFloat y = 0.0f;
+    
+    return CGRectMake(x, y, width, height);
+}
+
+#pragma mark - Button Action
+- (void)buttonAction:(id)sender
+{
+    UIButton *button = (UIButton *)sender;
+    
+    NSString *title = button.tag == 1 ? @"Button one pressed" : @"Button two pressed";
+    
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:title message:nil delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+    [alertView show];
+}
+
 #pragma mark - UITapGestureRecognizer action
 
 - (void)panOverlay:(id)sender
 {
     UIView *overlay = [sender view];
     
-    
     CGPoint translatedPoint = [(UIPanGestureRecognizer *)sender translationInView:self];
     
     if([(UIPanGestureRecognizer*)sender state] == UIGestureRecognizerStateBegan) {
-        _firstX = [overlay center].x;
+        _firstX = [overlay center].x ;
         _firstY = [overlay center].y;
     }
     
     translatedPoint = CGPointMake(_firstX+translatedPoint.x, _firstY);
-    
-    CGFloat translation = translatedPoint.x - _firstX;
-    
-//    BOOL isPaningOutOfRightBound = overlay.center.x < _firstX;
-    
-//    if(isPaningOutOfRightBound){
-//        if(overlay.frame.origin.x > OVERLAY_MAX_TRANSLATION) {
-//            [overlay setCenter:CGPointMake(OVERLAY_MAX_TRANSLATION, _firstY)];
-//        }
-//        else{
-            [overlay setCenter:translatedPoint];
-//        }
-//    }
+    [overlay setCenter:translatedPoint];
     
     if([(UIPanGestureRecognizer *)sender state] == UIGestureRecognizerStateEnded){
-        BOOL isOverlayOutOfBounds = overlay.frame.origin.x > OVERLAY_MAX_TRANSLATION;
         
-        if(isOverlayOutOfBounds){
-            CGPoint position = CGPointMake(160.0f, _firstY);
-            [self animateOverlayToPosition:(CGPoint)position];
+        BOOL shouldPinOverlay = overlay.frame.origin.x > OVERLAY_PIN_POSITION_X;
+            
+        CGPoint position;
+        
+        if(shouldPinOverlay){
+           position = CGPointMake(self.center.x + OVERLAY_PIN_POSITION_X, _firstY);
+            _isOverlayPinned = YES;
+        }
+        else {
+            position = CGPointMake(self.center.x, _firstY);
         }
         
+        [self animateOverlayCenterToPosition:(CGPoint)position];
     }
-    
-//    CGFloat xTranslation = translatedPoint.x - _firstX;
-
-//    CGRect translatedFrame = [self overlayViewFrameWithHorizontalTranslation:xTranslation];
-//    
-//    [[sender view] setBounds:translatedFrame];
-
-    
-//    void (^animation) (void) = ^(void){
-//        _overlayView.frame = [self overlayViewFrameWithHorizontalTranslation:xTranslation];
-//    };
-//    
-//    void (^completions) (BOOL finished) = ^(BOOL finished){
-//    };
-//    
-//    [UIView animateWithDuration:0.5f
-//                          delay:0.0f
-//                        options:UIViewAnimationOptionCurveEaseInOut
-//                     animations:animation
-//                    completion:completions
-//     ];
 }
 
-- (void)animateOverlayToPosition:(CGPoint)position
+- (void)animateOverlayCenterToPosition:(CGPoint)position
 {
     void (^animation) (void) = ^(void){
         _overlayView.center = position;
@@ -185,9 +212,21 @@
 
     NSLog(@"%f  %f", velocity.y, velocity.x);
     
-    BOOL panningHorizontal = (fabs(velocity.x) > fabs(velocity.y)) ? YES : NO;
-        
-    return panningHorizontal;
+    BOOL isPanningHorizontally = (fabs(velocity.x) > fabs(velocity.y)) ? YES : NO;
+    
+    BOOL isPanningToRightSide = velocity.x > 0 ? YES : NO;
+    
+    BOOL gestureRecognizerShouldBegin;
+
+    if(_isOverlayPinned) {
+        gestureRecognizerShouldBegin = isPanningHorizontally;
+        _isOverlayPinned = NO;
+    }
+    else {
+        gestureRecognizerShouldBegin = isPanningHorizontally && isPanningToRightSide;
+    }
+    
+    return gestureRecognizerShouldBegin;
 }
 
 
