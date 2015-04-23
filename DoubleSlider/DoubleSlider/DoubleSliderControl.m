b57dfcf98c36c54c42e486de3eafc8396fcaa3b0
Hari Kunwar <hkunwar@phunware.com>
2015-04-23 10:54:58 -0700

    Added ability to pan sliders using constraints.

--- DoubleSlider/DoubleSlider/DoubleSliderControl.m
+++ DoubleSlider/DoubleSlider/DoubleSliderControl.m
@@ -8,12 +8,24 @@
 
 #import "DoubleSliderControl.h"
 
+@interface DoubleSliderControl () <UIGestureRecognizerDelegate>
+
+@end
+
 @implementation DoubleSliderControl {
     __weak IBOutlet UIImageView *_sliderTrackBackground;
     __weak IBOutlet UIButton *_leftSlider;
     __weak IBOutlet UIButton *_rightSlider;
     __weak IBOutlet NSLayoutConstraint *_leftSliderLeadingConstraint;
     __weak IBOutlet NSLayoutConstraint *_rightSliderLeadingConstraint;
+    UIPanGestureRecognizer *_leftSliderPanGestureRecognizer;
+    UIPanGestureRecognizer *_rightSliderPanGestureRecognizer;
+    CGPoint _leftSliderGestureStartingCenter;
+    CGPoint _rightSliderGestureStartingCenter;
+    
+    CGPoint _leftSliderGestureStartingOrigin;
+    CGPoint _rightSliderGestureStartingOrigin;
+
 }
 
 - (void)awakeFromNib {
@@ -24,12 +36,45 @@
     sliderView.layer.borderWidth = 3.0f;
     
     [self addSubview:sliderView];
-}
-
-- (void)layoutSubviews {
-    [super layoutSubviews];
     
+    
+    _leftSliderPanGestureRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(panLeftSlider:)];
+    _leftSliderPanGestureRecognizer.delegate = self;
+    [_leftSlider addGestureRecognizer:_leftSliderPanGestureRecognizer];
+    
+    _rightSliderPanGestureRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(panRightSlider:)];
+    _rightSliderPanGestureRecognizer.delegate = self;
+    [_rightSlider addGestureRecognizer:_rightSliderPanGestureRecognizer];
 }
 
+
+- (void)panLeftSlider:(UIPanGestureRecognizer *)panGestureRecognizer {
+    UIGestureRecognizerState gestureState = [panGestureRecognizer state];
+    
+    if(gestureState == UIGestureRecognizerStateBegan) {
+        _leftSliderGestureStartingCenter = _leftSlider.center ;
+        _leftSliderGestureStartingOrigin = _leftSlider.frame.origin;
+    }
+    
+    CGPoint translatedPoint = [panGestureRecognizer translationInView:self];
+    CGPoint translatedOrigin = CGPointMake(_leftSliderGestureStartingOrigin.x + translatedPoint.x, _leftSliderGestureStartingOrigin.y);
+    
+    _leftSliderLeadingConstraint.constant = translatedOrigin.x;
+    
+    _rightSliderLeadingConstraint.constant = _rightSlider.frame.origin.x;
+}
+
+- (void)panRightSlider:(UIPanGestureRecognizer *)panGestureRecognizer {
+    UIGestureRecognizerState gestureState = [panGestureRecognizer state];
+    
+    if(gestureState == UIGestureRecognizerStateBegan) {
+        _rightSliderGestureStartingOrigin = _rightSlider.frame.origin ;
+    }
+    
+    CGPoint translatedPoint = [panGestureRecognizer translationInView:self];
+    CGPoint translatedOrigin = CGPointMake(_rightSliderGestureStartingOrigin.x + translatedPoint.x, _rightSliderGestureStartingOrigin.y);
+    
+    _rightSliderLeadingConstraint.constant = translatedOrigin.x;
+}
 
 @end
